2a5c441648a9004b0ab5e8231255efaab787d1d9
Jeff Deville <jeffdeville@gmail.com>
2021-10-07 10:17:36 -0400

    implement scoring algorithm

--- lib/lineups/scoring.ex
+++ lib/lineups/scoring.ex
@@ -1,82 +1,69 @@
-@type positions :: :goalie | :def | :stopper | :def_mid | :off_mid | :fwd
-GOALIE = 0
-DEF = 1
-STOPPER = 2
-DEF_MID = 3
-OFF_MID = 4
-FWD = 5
+defmodule Lineups.Scoring do
+  @num_players_on_field 7
+  @players %{
+      Breccan: [0, 4, 4, 5, 4, 4, 2],
+      Cameron: [5, 2, 2, 1, 2, 3, 3],
+      Evan: [3, 4, 4, 2, 2, 3, 3],
+      Harry: [3, 4, 4, 2, 2, 3, 3],
+      Isaiah: [0, 3, 3, 2, 4, 3, 3],
+      Jack: [5, 5, 5, 3, 4, 4, 4],
+      Linsana: [0, 5, 5, 5, 5, 5, 5],
+      Lusaine: [0, 5, 5, 5, 5, 5, 5],
+      Paco: [0, 3, 3, 2, 3, 4, 2],
+      Richard: [0, 2, 2, 1, 2, 1, 1],
+      Ryan: [0, 3, 3, 1, 3, 2, 2],
+      SamK: [0, 2, 2, 1, 3, 2, 2],
+      SamS: [0, 4, 4, 5, 4, 4, 2],
+    }
 
-players = [
-  %{
-    name: "Breccan",
-    scores: [0, 4, 5, 4, 4, 2]
-  },
-  %{
-    name: "Harry",
-    scores: [3, 4, 2, 2, 3, 3]
-  },
-  %{
-    name: "Paco",
-    scores: [0, 3, 2, 3, 4, 2]
-  },
-  %{
-    name: "SamK",
-    scores: [0, 2, 1, 3, 2, 2]
-  },
-  %{
-    name: "SamS",
-    scores: [0, 4, 5, 4, 4, 2]
-  },
-  %{
-    name: "Evan",
-    scores: [3, 4, 2, 2, 3, 3]
-  },
-  %{
-    name: "Jack",
-    scores: [5, 5, 3, 4, 4, 4]
-  },
-  %{
-    name: "Richard",
-    scores: [0, 2, 1, 2, 1, 1]
-  },
-  %{
-    name: "Linsana",
-    scores: [0, 5, 5, 5, 5, 5]
-  },
-  %{
-    name: "Lusaine",
-    scores: [0, 5, 5, 5, 5, 5]
-  },
-  %{
-    name: "Ryan",
-    scores: [0, 3, 1, 3, 2, 2]
-  },
-  %{
-    name: "Isaiah",
-    scores: [0, 3, 2, 4, 3, 3]
-  },
-  %{
-    name: "Cameron",
-    scores: [5, 2, 1, 2, 3, 3]
-  }
-]
-
-defmodule Lineups.Player do
-  defstruct name: "", rankings: %{}
-end
-
-defmodule Lineups.Lineup do
-  # defstruct positions: Player[]
-end
-
-defmodule(Lineups.Scoring) do
-  defstruct name: "John", age: 27
-
-  def score(lineups, players) do
-    lineups
-    |> Enum.map(&score_lineup(&1, players))
+  @spec score(list(list(atom()))) :: any
+  def score([]), do: 0
+  def score(lineups) do
+    if any_invalid_lineups?(lineups) do
+      0
+    else
+      lineups
+      |> Enum.reduce(0, fn lineup, acc -> acc + score_lineup(lineup) + score_defense(lineup) end)
+      |> average_score(lineups)
+    end
   end
 
-  defp score_lineup(%Lineup{} = lineup, players) do
+  defp score_lineup(lineup) do
+    # a Lineup is just an list of players, their position inferred by index
+    lineup
+    |> Stream.with_index(0)
+    |> Enum.map(fn { player, index} -> Enum.at(@players[player],index) end)
+    |> Enum.reduce(0, fn score, acc -> acc + score end )
+    |> Integer.pow(2)
+  end
+
+  defp score_defense(lineup) do
+    [_, def1, def2 | _tail] = lineup
+    Integer.pow(Enum.at(@players[def1], 1) + Enum.at(@players[def2], 2), 2)
+  end
+
+  defp average_score(total_score, lineups), do: total_score / length(lineups)
+
+  defp any_invalid_lineups?(lineups) do
+    lineups
+    |> Enum.any?(fn lineup -> any_duplicate_players_or_incomplete_lineups?(lineup) or any_missing_players?(lineup) end)
+  end
+
+  defp any_missing_players?(lineup) do
+    if Enum.any?(lineup, fn player -> !Map.has_key?(@players, player) end) do
+        IO.inspect(lineup, label: "Missing players")
+        true
+    else
+      false
+    end
+  end
+
+  defp any_duplicate_players_or_incomplete_lineups?(lineup) do
+    if MapSet.size(MapSet.new(lineup)) != @num_players_on_field do
+      IO.inspect(lineup, label: "duplicate or incomplete lineup")
+      true
+    else
+      false
+    end
   end
 end
--- test/lineups/scoring_test.exs
+++ test/lineups/scoring_test.exs
@@ -1,7 +1,39 @@
 defmodule Lineups.ScoringTest do
   use ExUnit.Case, async: true
+  # alias Lineups.Scoring
+  describe "invalid lineups score 0" do
+    test "empty lineups score 0" do
+      assert Lineups.Scoring.score([]) == 0
+    end
 
-  test "scoring" do
-    assert true
+    test "incomplete lineups score 0" do
+      assert Lineups.Scoring.score([[:Breccan, :Harry, :Isaiah ]]) == 0
+    end
+
+    test "any incomplete lineup scores 0" do
+      assert Lineups.Scoring.score([[:Breccan, :Harry, :Isaiah, :Cameron, :Paco, :Ryan, :Evan], [:Breccan, :Harry]]) == 0
+    end
+
+    test "lineups with duplicate players scores 0" do
+      assert Lineups.Scoring.score([[:Breccan, :Breccan, :Breccan, :Breccan, :Breccan, :Breccan, :Breccan]]) == 0
+    end
+
+    test "lineups with invalid players scores 0" do
+      assert Lineups.Scoring.score([[:ThisIsNotAPlayer, :Harry, :Isaiah, :Cameron, :Paco, :Ryan, :Evan]]) == 0
+    end
+
+  end
+
+  describe "valid lineups generate scores" do
+    test "a single lineup is scored properly" do
+      assert Lineups.Scoring.score([[:Breccan, :Harry, :Isaiah, :Cameron, :Paco, :Ryan, :Evan]]) == 305
+    end
+
+    test "lineup scores are averaged, so duplicate lineups should have the same score" do
+      lineup1 = [:Breccan, :Harry, :Isaiah, :Cameron, :Paco, :Ryan, :Evan]
+      lineup2 = [:Cameron, :Evan, :Richard, :Breccan, :SamK, :Lusaine, :Linsana]
+      assert Lineups.Scoring.score([ lineup1, lineup1 ]) == Lineups.Scoring.score([ lineup1])
+      assert Lineups.Scoring.score([ lineup1, lineup2 ]) == 591
+    end
   end
 end
