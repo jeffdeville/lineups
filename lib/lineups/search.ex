b2b010d46e6141ca43ab129b568476a666a45007
Jeff Deville <jeffdeville@gmail.com>
2021-10-12 12:33:46 -0400

    Search / Scoring working for a single lineup

--- lib/lineups/search.ex
+++ lib/lineups/search.ex
@@ -1,76 +1,144 @@
 defmodule Lineups.Search do
-# goalie
-# defense
-# offense
-# speed
-# endurance
-# awareness
-# not_here
-@position_skills Nx.tensor(
-    [
-      # goalie
-      [1, 0, 0, 0, 0, 0, 0],
-      # def1
-      [0, 1, 0, 0, 0, 0, 0],
-      # def2
-      [0, 1, 0, 0, 0, 0, 0],
-      # stopper
-      [0, 1, 0.3, 1, 1, 1, 0],
-      # def_mid
-      [0, 1, 0.6, 1, 0.7, 0.4, 0],
-      # off_mid
-      [0, 0.4, 1, 1, 1, 0.9, 0],
-      # fwd
-      [0, 0, 1, 0.5, 1, 1, 0],
-      # sub1
-      [0, 0, 0, 0, 0, 0, 1],
-      # sub2
-      [0, 0, 0, 0, 0, 0, 1],
-      # sub3
-      [0, 0, 0, 0, 0, 0, 1],
-      # sub4
-      [0, 0, 0, 0, 0, 0, 1],
-      # sub5
-      [0, 0, 0, 0, 0, 0, 1],
-      # sub6
-      [0, 0, 0, 0, 0, 0, 1]
-    ],
-    names: [:position, :skill_weighting]
-  )
+  # goalie
+  # defense
+  # offense
+  # speed
+  # endurance
+  # awareness
+  @position_skills Nx.tensor(
+                     [
+                       # goalie
+                       [1, 0, 0, 0, 0, 0],
+                       # def1
+                       [0, 1, 0, 0, 0, 0],
+                       # def2
+                       [0, 1, 0, 0, 0, 0],
+                       # stopper
+                       [0, 1, 0.3, 1, 1, 1],
+                       # def_mid
+                       [0, 1, 0.6, 1, 0.7, 0.4],
+                       # off_mid
+                       [0, 0.4, 1, 1, 1, 0.9],
+                       # fwd
+                       [0, 0, 1, 0.5, 1, 1],
+                       # sub1
+                       [0, 0, 0, 0, 0, 0],
+                       # sub2
+                       [0, 0, 0, 0, 0, 0],
+                       # sub3
+                       [0, 0, 0, 0, 0, 0],
+                       # sub4
+                       [0, 0, 0, 0, 0, 0],
+                       # sub5
+                       [0, 0, 0, 0, 0, 0],
+                       # sub6
+                       [0, 0, 0, 0, 0, 0]
+                     ],
+                     names: [:position, :skill_weighting]
+                   )
+  @max_skill Nx.broadcast(5, {6})
+  @players %{
+    0 => :Breccan,
+    1 => :Cameron,
+    2 => :Evan,
+    3 => :Harry,
+    4 => :Isaiah,
+    5 => :Jack,
+    6 => :Linsana,
+    7 => :Lusaine,
+    8 => :Paco,
+    9 => :Richard,
+    10 => :Ryan,
+    11 => :SamK,
+    12 => :SamS
+  }
+
+  @positions %{
+    0 => :goalie,
+    1 => :def1,
+    2 => :def2,
+    3 => :stopper,
+    4 => :def_mid,
+    5 => :off_mid,
+    6 => :fwd
+  }
 
   # TODO: I should send in the list of kids
   # who are playing here, so that I can cut down the number of positions and whatnot that are needed. (Remove positions from the end of the list, and just assume that if the number of real positions is greater than the number of players, that you're in an invalid state anyway)
   # unneeded data is dropped
   # @spec init(map(string, list(number)), any) :: nil
-  def init(position_weightings, num_periods) do
-    {num_positions, _} = position_weightings |> Nx.shape
+  def init(player_skills, num_periods) do
+    {num_players, _} = player_skills |> Nx.shape()
 
     game_lineup =
-      0..(num_periods-1)
+      0..(num_periods - 1)
       |> Enum.to_list()
-      |> Enum.map(fn _ -> Nx.eye({num_positions, num_positions}) end)
+      |> Enum.map(fn _ -> Nx.eye({num_players, num_players}) end)
       |> Nx.stack()
 
     game_lineup
   end
 
-  def search(current_lineups, _current_lineups_score, _player_skills, _position_weights, total_iterations, current_iteration) when total_iterations == current_iteration do
+  def search(
+        current_lineups,
+        _current_lineups_score,
+        _player_skills,
+        total_iterations,
+        current_iteration
+      )
+      when total_iterations == current_iteration do
     current_lineups
   end
 
-  def search(current_lineups, current_lineups_score, player_skills,position_weights, total_iterations, current_iteration) do
+  def search(
+        current_lineups,
+        current_lineups_score,
+        player_skills,
+        total_iterations,
+        current_iteration
+      ) do
     new_state = evolve(current_lineups)
-    new_score = score(new_state)
-    {best_lineup, best_score} = if new_score > current_lineups_score do
-      {new_state, new_score}
-    else
-      {current_lineups, current_lineups_score}
-    end
-    search(best_lineup, best_score, player_skills, position_weights, total_iterations, current_iteration + 1)
+    new_score = score(new_state, player_skills)
+    # IO.inspect({current_lineups_score, new_score}, label: "\tscores")
+    {best_lineup, best_score} =
+      if new_score > current_lineups_score do
+        IO.inspect({current_lineups_score, new_score}, label: "Current, New Score")
+        print(new_state)
+        {new_state, new_score}
+      else
+        {current_lineups, current_lineups_score}
+      end
+
+    search(
+      best_lineup,
+      best_score,
+      player_skills,
+      total_iterations,
+      current_iteration + 1
+    )
   end
 
-  def score(_lineups) do
-    0.5
+  def score(lineups, player_skills) do
+    {_num_periods, num_players, _} = Nx.shape(lineups)
+    {_, num_skills} = Nx.shape(@position_skills)
+
+    # yields a player / position of scores for each player in each position
+    player_ratings =
+      0..(num_players - 1)
+      |> Enum.to_list()
+      |> Enum.map(fn position ->
+        player_skills
+        |> Nx.transpose()
+        |> Nx.multiply(
+          Nx.broadcast(@position_skills[position], {num_skills, num_players}, axes: [0])
+        )
+        |> Nx.sum(axes: [0])
+        # |> Nx.divide(@position_skills[position] |> Nx.multiply(@max_skill) |> Nx.sum())
+      end)
+      |> Nx.stack()
+      |> Nx.transpose()
+
+    Nx.multiply(Nx.broadcast(player_ratings, lineups), lineups) |> Nx.sum() |> Nx.to_scalar()
   end
 
   def evolve(current_lineups) do
@@ -78,28 +146,54 @@ defmodule Lineups.Search do
     {num_periods, num_positions, _} = Nx.shape(current_lineups)
 
     # which period am I changing
-    period = Enum.random(0..num_periods-1)
+    period = Enum.random(0..(num_periods - 1))
 
     # which kids' positions to swap
-    all_positions = 0..(num_positions-1) |> Enum.to_list()
+    all_positions = 0..(num_positions - 1) |> Enum.to_list()
     [pos1, pos2] = all_positions |> Enum.take_random(2)
     new_position_indices = swap(all_positions, pos1, pos2)
     mutate(current_lineups, period, new_position_indices)
   end
 
-  def mutate(current_lineups, period, new_position_indices) do\
+  def mutate(current_lineups, period, new_position_indices) do
     {num_periods, _, _} = Nx.shape(current_lineups)
 
     # reposition players
     new_lineup = Nx.take(current_lineups[period], Nx.tensor(new_position_indices))
 
-    lineup_list = (0..num_periods-1)
+    lineup_list =
+      0..(num_periods - 1)
       |> Enum.map(fn
         ^period -> new_lineup
         index -> current_lineups[index]
       end)
 
     Nx.stack(lineup_list)
+  end
+
+  def collect_lineups(current_lineups) do
+    {num_periods, num_players, _num_positions} = Nx.shape(current_lineups)
+
+    0..(num_periods - 1)
+    |> Enum.reduce([], fn period, acc ->
+      lineup =
+        0..(num_players - 1)
+        |> Enum.reduce(%{}, fn player, acc ->
+          position_index =
+            current_lineups[period][player]
+            |> Nx.to_flat_list()
+            |> Enum.find_index(&(&1 == 1))
+
+          position = @positions[position_index]
+          Map.put(acc, @players[player], position)
+        end)
+
+      [acc | lineup]
+    end)
+  end
+
+  def print(current_lineups) do
+    IO.inspect(collect_lineups(current_lineups))
   end
 
   defp swap(a, i1, i2) do
--- test/lineups/search_test.exs
+++ test/lineups/search_test.exs
@@ -1,57 +1,225 @@
 defmodule Lineups.SearchTest do
   use ExUnit.Case, async: true
   alias Lineups.Search
+
+  @player_skills Nx.tensor([
+                   # Breccan
+                   [0, 5, 4, 5, 4, 5],
+                   # Cameron
+                   [5, 3, 3, 3, 3, 3],
+                   # Evan
+                   [4, 4, 4, 2, 2, 4],
+                   # Harry
+                   [3, 4, 3, 4, 3, 2],
+                   # Isaiah
+                   [0, 4, 3, 4, 4, 3],
+                   # Jack
+                   [4, 5, 4, 3, 3, 4],
+                   # Linsana
+                   [0, 5, 5, 5, 5, 5],
+                   # Lusaine
+                   [0, 5, 5, 5, 5, 5],
+                   # Paco
+                   [0, 3, 3, 5, 3, 3],
+                   # Richard
+                   [0, 2, 1, 2, 1, 1],
+                   # Ryan
+                   [0, 2, 2, 2, 1, 3],
+                   # SamK
+                   [0, 3, 4, 4, 4, 3],
+                   # SamS
+                   [0, 2, 3, 4, 5, 2]
+                 ])
   @position_skills Nx.tensor(
-  [
-    [1, 0, 0, 0, 0, 0, 0], # goalie
-    [0, 1, 0, 0, 0, 0, 0], # def1
-    [0, 1, 0, 0, 0, 0, 0], # def2
-    [0, 1, 0.3, 1, 1, 1, 0], # stopper
-    [0, 1, 0.6, 1, 0.7, 0.4, 0], # def_mid
-    [0, 0.4, 1, 1, 1, 0.9, 0], # off_mid
-    [0, 0, 1, 0.5, 1, 1, 0], # fwd
-    [0, 0, 0, 0, 0, 0, 1], # sub1
-    [0, 0, 0, 0, 0, 0, 1], # sub2
-    [0, 0, 0, 0, 0, 0, 1], # sub3
-    [0, 0, 0, 0, 0, 0, 1], # sub4
-    [0, 0, 0, 0, 0, 0, 1], # sub5
-    [0, 0, 0, 0, 0, 0, 1], # sub6
-  ],
-  names: [:position, :skill_weighting]
-  )
+                     [
+                       # goalie
+                       [1, 0, 0, 0, 0, 0],
+                       # def1
+                       [0, 1, 0, 0, 0, 0],
+                       # def2
+                       [0, 1, 0, 0, 0, 0],
+                       # stopper
+                       [0, 1, 0.3, 1, 1, 1],
+                       # def_mid
+                       [0, 1, 0.6, 1, 0.7, 0.4],
+                       # off_mid
+                       [0, 0.4, 1, 1, 1, 0.9],
+                       # fwd
+                       [0, 0, 1, 0.5, 1, 1],
+                       # sub1
+                       [0, 0, 0, 0, 0, 0],
+                       # sub2
+                       [0, 0, 0, 0, 0, 0],
+                       # sub3
+                       [0, 0, 0, 0, 0, 0],
+                       # sub4
+                       [0, 0, 0, 0, 0, 0],
+                       # sub5
+                       [0, 0, 0, 0, 0, 0],
+                       # sub6
+                       [0, 0, 0, 0, 0, 0]
+                     ],
+                     names: [:position, :skill_weighting]
+                   )
+  @breccan 0
+  @cameron 1
+  @evan 2
+  @harry 3
+  @isaiah 4
+  @jack 5
+  @linsana 6
+  @lusaine 7
+  @paco 8
+  @richard 9
+  @ryan 10
+  @samK 11
+  @samS 12
+
+  @nopos Nx.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+  @goalie Nx.tensor([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+  @def1 Nx.tensor([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+  @def2 Nx.tensor([0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+  @stopper Nx.tensor([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+  @def_mid Nx.tensor([0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+  @off_mid Nx.tensor([0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+  @fwd Nx.tensor([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0])
+  @sub1 Nx.tensor([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
+  @sub2 Nx.tensor([0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0])
+  @sub3 Nx.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+  @sub4 Nx.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0])
+  @sub5 Nx.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0])
+  @sub6 Nx.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+
   describe "init" do
     test "creates the initial board with the number of positions from the weighting map, and the num periods passed in" do
-      lineups = Search.init(@position_skills, 3)
+      lineups = Search.init(@player_skills, 3)
       assert Nx.shape(lineups) == {3, 13, 13}
 
-      lineups = Search.init(@position_skills, 6)
+      lineups = Search.init(@player_skills, 6)
       assert Nx.shape(lineups) == {6, 13, 13}
     end
   end
 
   describe "search" do
     test "when num iterations is reached, return current state" do
-      lineups = Search.init(@position_skills, 3)
-      new_lineups = Search.search(lineups, 0, nil, @position_skills, 10, 10)
+      lineups = Search.init(@player_skills, 3)
+      new_lineups = Search.search(lineups, 0, @player_skills, 10, 10)
 
       assert new_lineups == lineups
     end
 
     test "when num iterations not completed, search" do
-      lineups = Search.init(@position_skills, 3)
-      new_lineups = Search.search(lineups, 0, nil, @position_skills, 10, 0)
+      lineups = Search.init(@player_skills, 1)
+      initial_score = Search.score(lineups, @player_skills)
+      new_lineups = Search.search(lineups, initial_score, @player_skills, 10000, 0)
+      Search.print(new_lineups)
+      assert Search.score(new_lineups, @player_skills) > Search.score(lineups, @player_skills)
+      assert false
+    end
+  end
 
-      assert new_lineups == lineups
+  describe "evolve" do
+    test "randomizes as expected" do
+      :rand.seed(:exsss, {100, 101, 102})
+      lineups = Search.init(@player_skills, 3)
+      new_lineups = Search.evolve(lineups)
+      assert lineups[0] == new_lineups[0]
+      assert lineups[2] == new_lineups[2]
+
+      assert lineups[1] !=
+               Nx.tensor([
+                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+               ])
     end
   end
 
   describe "mutate" do
     test "randomly alter a lineup in just 1 characteristic" do
-      lineups = Search.init(@position_skills, 3)
-
+      lineups = Search.init(@player_skills, 3)
       new_lineups = Search.mutate(lineups, 1, [0, 1, 3, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-      IO.inspect(new_lineups, limit: :infinity)
-      assert lineups != new_lineups
+      assert lineups[0] == new_lineups[0]
+      assert lineups[2] == new_lineups[2]
+
+      assert lineups[1] !=
+               Nx.tensor([
+                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+               ])
+    end
+  end
+
+  describe "score" do
+    test "scores correctly" do
+      lineups = Search.init(@player_skills, 3)
+      score = Search.score(lineups, @player_skills)
+      assert score == 7.569902420043945
+    end
+
+    def make_lineup(stuff) do
+      lineup =
+        0..12
+        |> Enum.map(fn player ->
+          player_pos =
+            Enum.find(stuff, fn
+              {^player, position} -> true
+              _ -> false
+            end)
+
+          if player_pos, do: elem(player_pos, 1), else: @nopos
+        end)
+        |> Nx.stack()
+        |> Nx.broadcast({1, 13, 13})
+    end
+
+    test "simple choose the better lineup" do
+      stopper = make_lineup([{@breccan, @stopper}])
+      goalie = make_lineup([{@breccan, @goalie}])
+      def1 = make_lineup([{@breccan, @def1}])
+
+      stopper_score = Search.score(stopper, @player_skills)
+      goalie_score = Search.score(goalie, @player_skills)
+      def1_score = Search.score(def1, @player_skills)
+
+      assert goalie_score == 0
+      assert def1_score < stopper_score
+    end
+
+    test "multi-player choose the better lineup" do
+      bad = make_lineup([{@breccan, @stopper}])
+      bad_score = Search.score(bad, @player_skills)
+
+      good =
+        make_lineup([
+          {@breccan, @stopper},
+          {@linsana, @fwd},
+          {@lusaine, @off_mid}
+        ])
+
+      good_score = Search.score(good, @player_skills)
+      assert good_score > bad_score
     end
   end
 end
